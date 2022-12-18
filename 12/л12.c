14de9f1d86fb9a01f3133193158309a2d0da191d
ktuzova <103954829+ktuzova@users.noreply.github.com>
2022-12-18 21:13:59 +0300

    Add files via upload

---
+++ 12/л12.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    printf("Input a number\n");
+    int a;
+    scanf("%d",&a);
+    int n = a;
+    int k = 0;
+    int second = 0;
+    int first = 0;
+    int last = a%10;
+    while (n>0){
+        second = first;
+        first = n%10;
+        k++;
+        n/=10;
+    }
+    k-=2;
+    
+    a = a - last + second + (last - second)*pow(10,k);
+    printf("%d",a);
+    return 0;
+}
