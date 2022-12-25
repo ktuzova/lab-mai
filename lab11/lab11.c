8ab41f52f842c424472d4862bd0b6d1561bb7a54
ktuzova <103954829+ktuzova@users.noreply.github.com>
2022-12-25 15:37:49 +0300

    Add files via upload

---
+++ lab11/lab11.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char c;
+    while ((c = getchar()) != EOF) {
+        if (c=="\n"){
+            printf(' ');
+        }
+        else{
+            printf("%s", c);
+        }
+    }
+    return 0;
+}
